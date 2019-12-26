395cc870e85f87190a6d9ff979015a06e6759169
czdxn <czdxn>
2019-12-26 15:11:50 +0800

    flutter

--- lib/home.dart
+++ lib/home.dart
@@ -64,4 +64,120 @@ class _MyTestAppState extends State<MyTestApp> {
       ),
     );
   }
+
+/*
+
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              color: Colors.black,
+              child: new Transform(
+                alignment: Alignment.topRight, //相对于坐标系原点的对⻬⽅式
+                transform: new Matrix4.skewY(0.3), //沿Y轴倾斜0.3弧度
+                child: new Container(
+                  padding: const EdgeInsets.all(8.0),
+                  color: Colors.deepOrange,
+                  child: const Text('Apartment for rent!'),
+                ),
+              ),
+            ),
+            Icon(Icons.ac_unit),
+            Icon(Icons.airport_shuttle),
+            Icon(Icons.all_inclusive),
+            Icon(Icons.beach_access),
+            Icon(Icons.cake),
+            Icon(Icons.free_breakfast),
+            Wrap(
+                spacing: 8.0, // 主轴(⽔平)⽅向间距
+                alignment: WrapAlignment.start,
+                runSpacing: 4.0, // 纵轴（垂直）⽅向间距
+                children: <Widget>[
+                  new Chip(
+                      label: new Text("123rwraasdasdaasdasdar123"),
+                      avatar: new CircleAvatar(
+                          backgroundColor: Colors.blue, child: Text('A'))),
+                  new Chip(
+                      label: new Text("123asdas123"),
+                      avatar: new CircleAvatar(
+                          backgroundColor: Colors.blue, child: Text('A'))),
+                  new Chip(
+                      label: new Text("12asdasdasd3123"),
+                      avatar: new CircleAvatar(
+                          backgroundColor: Colors.blue, child: Text('A'))),
+                  new Chip(
+                      label: new Text("123123"),
+                      avatar: new CircleAvatar(
+                          backgroundColor: Colors.blue, child: Text('A')))
+                ]),
+            new Text(
+              'You have pushed the button this many times:',
+            ),
+            new Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+            new IconButton(
+              icon: Icon(Icons.thumb_up),
+              onPressed: _incrementCounter,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+   */
+
+/*
+ new CustomScrollView(
+        shrinkWrap: true,
+        // 内容
+        slivers: <Widget>[
+          new SliverPadding(
+            padding: const EdgeInsets.all(10.0),
+            sliver: new SliverList(
+              delegate: new SliverChildListDelegate(
+                <Widget>[
+                  new Card(),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10, bottom: 10),
+                    child: new Text("123"),
+                  ),
+                  new Divider(),
+                  const Text('A'),
+                  new Divider(),
+                  const Text('B'),
+                  new Divider(),
+                  const Text('C'),
+                  new Divider(),
+                  const Text('D'),
+                  new Divider(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+ */
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
@@ -34,73 +35,32 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              color: Colors.black,
-              child: new Transform(
-                alignment: Alignment.topRight, //相对于坐标系原点的对⻬⽅式
-                transform: new Matrix4.skewY(0.3), //沿Y轴倾斜0.3弧度
-                child: new Container(
-                  padding: const EdgeInsets.all(8.0),
-                  color: Colors.deepOrange,
-                  child: const Text('Apartment for rent!'),
+      body: ListView.builder(
+        itemCount: 50,
+        itemBuilder: (BuildContext context, int index) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Column(  // item
+                  children: <Widget>[
+                    Text(
+                      "title" + "$index",
+                      textAlign: TextAlign.start,
+                    ),
+                  ],
                 ),
               ),
-            ),
-            Icon(Icons.ac_unit),
-            Icon(Icons.airport_shuttle),
-            Icon(Icons.all_inclusive),
-            Icon(Icons.beach_access),
-            Icon(Icons.cake),
-            Icon(Icons.free_breakfast),
-            Wrap(
-                spacing: 8.0, // 主轴(⽔平)⽅向间距
-                alignment: WrapAlignment.start,
-                runSpacing: 4.0, // 纵轴（垂直）⽅向间距
-                children: <Widget>[
-                  new Chip(
-                      label: new Text("123rwraasdasdaasdasdar123"),
-                      avatar: new CircleAvatar(
-                          backgroundColor: Colors.blue, child: Text('A'))),
-                  new Chip(
-                      label: new Text("123asdas123"),
-                      avatar: new CircleAvatar(
-                          backgroundColor: Colors.blue, child: Text('A'))),
-                  new Chip(
-                      label: new Text("12asdasdasd3123"),
-                      avatar: new CircleAvatar(
-                          backgroundColor: Colors.blue, child: Text('A'))),
-                  new Chip(
-                      label: new Text("123123"),
-                      avatar: new CircleAvatar(
-                          backgroundColor: Colors.blue, child: Text('A')))
-                ]),
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            new IconButton(
-              icon: Icon(Icons.thumb_up),
-              onPressed: _incrementCounter,
-            ),
-          ],
-        ),
+              Divider(),
+            ],
+          );
+        },
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
